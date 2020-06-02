5ebdf6ec5fdb68de43faddb90d6275e3d8bdb2c4
Lucas  Rodrigues Estefano <42548142+lucasg13info@users.noreply.github.com>
2020-06-02 19:54:54 -0300

    Métodos POST, GET, PUT e DELETE

--- aulas/server/src/server.ts
+++ aulas/server/src/server.ts
@@ -1,19 +1,44 @@
-import express from 'express';
+import express, { request } from 'express';
 
 const app = express();
+
+
+//ROTA: ENDEREÇO COMPLETO DA REQUISIÇÃO
+//RECURSO: QUAL ENTIDADE ESTAMOS ACESSANDO DO SISTEMA
+
+
+//GET: Navegador só faz get; Buscar uma ou mais informações do back-end;
+//POST: Criar uma nova informação no back-end;
+//PUT: Atualizar uma informação existente no back-end;
+//DELETE: remover uma informação do back-end.
+
+//POST http://localhost:3333/users = Criar users
+ //GET http://localhost:3333/users = Listar usuários
+// GET http://localhost:3333/users/5 = Buscar dados do usuário com id 5
+
 
 app.get('/users', (requist, response) => {
     console.log('Listagem de usuários');
 
 // JSON
 
-    response.json([
+    return response.json([
         'Diego',
         'Cleiton',
         'Robson',
         'Daniel'
     ]);
 });
+
+
+app.post('/users', (request, response) => {
+    const user = {
+        name: 'Diego',
+        email: 'diego@rocketseat.com.br'
+    }
+
+    return response.json(user);
+})
 
 app.listen(3333);
 
