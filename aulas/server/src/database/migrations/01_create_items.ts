7086174e9a1ab2b57f50f7fe6524e18e6bed5ff5
Lucas  Rodrigues Estefano <42548142+lucasg13info@users.noreply.github.com>
2020-06-02 22:05:51 -0300

    Concluindo a criação das tabelas do banco e rodando o mesmo

--- aulas/server/src/database/migrations/01_create_items.ts
+++ aulas/server/src/database/migrations/01_create_items.ts
@@ -1,3 +1,19 @@
-export async function up(){}
+//export async function up(){}
 
-export async function down(){}
+//export async function down(){}
+import Knex from 'knex';
+
+export async function up(knex: Knex){
+    //CRIAR TABELA
+    return knex.schema.createTable('items', table  => {
+        table.increments('id').primary();
+        table.string('image').notNullable();
+        table.string('titulo').notNullable();
+    })
+
+
+}
+
+export async function down(knex: Knex){
+   return knex.schema.dropTable('items');
+}
