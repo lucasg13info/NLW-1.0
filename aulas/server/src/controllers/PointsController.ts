d19718e0be74b8810c78871d1163fc2d0bd0a658
Lucas  Rodrigues Estefano <42548142+lucasg13info@users.noreply.github.com>
2020-06-03 01:19:37 -0300

    Finalido a criação das rotas =, instalado o cors

--- aulas/server/src/controllers/PointsController.ts
+++ aulas/server/src/controllers/PointsController.ts
@@ -9,9 +9,20 @@ class PointsController {
 
             console.log(city, uf, items);
 
-            const parsedItems = String(items).split(',').map(item => Number(item.trim()))
+            const parsedItems = String(items)
+            .split(',')
+            .map(item => Number(item.trim()));
 
-            return response.json({ok: true})
+            const points = await Knex('points')
+            .join('point_items', 'points.id','=', 'point_items.point_id' )
+            .whereIn('point_items.item_id', parsedItems)
+            .where('city',String(city))
+            .where('uf', String(uf))
+            .distinct()
+            .select('points.*');
+
+            
+            return response.json(points);
     } 
 
     async show(request:Request, response: Response){
@@ -72,6 +83,8 @@ class PointsController {
         })
     
         await trx('point_items').insert(pointItems);
+
+        await trx.commit()
     
         return response.json({
             id: point_id,
