c06042225e4d3b7ae205685244082a2d07233f89
Lucas  Rodrigues Estefano <42548142+lucasg13info@users.noreply.github.com>
2020-06-02 23:51:05 -0300

    Inserindo dados no banco

--- aulas/server/src/routes.ts
+++ aulas/server/src/routes.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { response } from 'express';
 import Knex from './database/connection'
 
 const routes = express.Router();
@@ -9,6 +9,7 @@ routes.get('/items', async (request, response) => {
 
     const serializedItems = items.map(item =>{
         return {
+            id: item.id,
             title: item.title,
             image_url: `http://localhost:3333/uploads/${item.image}`,
         };
@@ -16,5 +17,30 @@ routes.get('/items', async (request, response) => {
     return response.json({serializedItems})
 });
 
+routes.post('/points', async (request, response) => {
+    const {
+        name,
+        email,
+        whatsapp,
+        latitude,
+        longitude,
+        city,
+        uf,
+        items
+    } = request.body;
+
+   await Knex('points').insert({
+        image: 'image-fake',   
+        name,
+        email,
+        whatsapp,
+        latitude,
+        longitude,
+        city,
+        uf
+    })
+
+    return response.json({sucess: true});
+});
 
 export default routes;
