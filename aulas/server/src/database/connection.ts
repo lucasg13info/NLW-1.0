82ced9a279d551206842ee63e903aba0827a379d
Lucas  Rodrigues Estefano <42548142+lucasg13info@users.noreply.github.com>
2020-06-02 21:08:06 -0300

    Concluido a conexão com o banco de dados

--- aulas/server/src/database/connection.ts
+++ aulas/server/src/database/connection.ts
@@ -1,11 +1,11 @@
 import knex from 'knex';
 import path from 'path';
 
-
-
 const connection = knex({
     client: 'sqlite3',
     connection: {
-        filename: path.resolve()
+        filename: path.resolve(__dirname,   'database.sqlite'),
     },
 });
+
+export default connection;
