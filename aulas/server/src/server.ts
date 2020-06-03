b4fc7885f3b31d27ac0940a86a0c4a3c53f52bbb
Lucas  Rodrigues Estefano <42548142+lucasg13info@users.noreply.github.com>
2020-06-02 21:00:09 -0300

    Criando a pasta de rotas

--- aulas/server/src/server.ts
+++ aulas/server/src/server.ts
@@ -1,65 +1,10 @@
-import express, { request } from 'express';
+import express from 'express';
+
+import routes from './routes';
 
 const app = express();
 
 app.use(express.json());
-//ROTA: ENDEREÇO COMPLETO DA REQUISIÇÃO
-//RECURSO: QUAL ENTIDADE ESTAMOS ACESSANDO DO SISTEMA
-
-
-//GET: Navegador só faz get; Buscar uma ou mais informações do back-end;
-//POST: Criar uma nova informação no back-end;
-//PUT: Atualizar uma informação existente no back-end;
-//DELETE: remover uma informação do back-end.
-
-//POST http://localhost:3333/users = Criar users
- //GET http://localhost:3333/users = Listar usuários
-// GET http://localhost:3333/users/5 = Buscar dados do usuário com id 5
-
-
-// Request Param: Parametros que vem na  própria rota qe identificam um recurso 
-// Query Param: Parametros que vem na própria rota geralmente opcionais para filtros, paginação.
-// Request body: parametros para criação/atualizaão de informações, nome, endereço, senha...
-
-const users = [
-    'Diego',
-    'Cleiton',
-    'Robson',
-    'Daniel'
-];
-
-
-app.get('/users', (request, response) => {
-    const search = String(request.query.search);
-
-
-    const filteredUsers = search ? users.filter(user => user.includes(search)) : users;
-
-
-    return response.json(filteredUsers);
-});
-
-app.get('/users/:id',(request, response) => {
-    const id = Number(request.params.id);
-
-    const user = users[id];
-
-    return response.json(user);
-})
-
-
-app.post('/users', (request, response) => {
-    const data = request.body;
-
-
-    
-    const user = {
-        name: data.name,
-        email: data.email,
-    }
-
-    return response.json(user);
-})
+app.use(routes);
 
 app.listen(3333);
-
