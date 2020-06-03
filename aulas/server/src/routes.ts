77dfbb20337cc751d3a4ff262a5acbc03f9e7b77
Lucas  Rodrigues Estefano <42548142+lucasg13info@users.noreply.github.com>
2020-06-03 00:30:40 -0300

    Semantica

--- aulas/server/src/routes.ts
+++ aulas/server/src/routes.ts
@@ -1,24 +1,14 @@
 import express, { response } from 'express';
-import Knex from './database/connection'
 
 import PointsController from './controllers/PointsController'; 
+import ItemsController from './controllers/ItemsController';
 
 const routes = express.Router();
 const pointsController = new PointsController();
+const itemsController = new ItemsController();
 
-routes.get('/items', async (request, response) => {
-    const items = await Knex('items').select('*');
 
-    const serializedItems = items.map(item =>{
-        return {
-            id: item.id,
-            title: item.title,
-            image_url: `http://localhost:3333/uploads/${item.image}`,
-        };
-    });
-    return response.json({serializedItems})
-});
-
+routes.get('/items', itemsController.index);
 routes.post('/points', pointsController.create );
 
 export default routes;
