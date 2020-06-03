81eacf13b4ecd338ee0dd7f577caf31c4ebe0a8a
Lucas  Rodrigues Estefano <42548142+lucasg13info@users.noreply.github.com>
2020-06-03 00:16:13 -0300

    Semantica

--- aulas/server/src/routes.ts
+++ aulas/server/src/routes.ts
@@ -17,43 +17,6 @@ routes.get('/items', async (request, response) => {
     return response.json({serializedItems})
 });
 
-routes.post('/points', async (request, response) => {
-    const {
-        name,
-        email,
-        whatsapp,
-        latitude,
-        longitude,
-        city,
-        uf,
-        items
-    } = request.body;
-
-    const trx = await Knex.transaction();
-
-   const insertedIds = await trx('points').insert({
-        image: 'image-fake',   
-        name,
-        email,
-        whatsapp,
-        latitude,
-        longitude,
-        city,
-        uf
-    })
-
-    const point_id = insertedIds[0];
-
-    const pointItems = items.map((item_id: Number) => {
-        return {
-            item_id,
-            point_id,
-        }
-    })
-
-    await trx('point_items').insert(pointItems);
-
-    return response.json({sucess: true});
-});
+routes.post('/points', );
 
 export default routes;
