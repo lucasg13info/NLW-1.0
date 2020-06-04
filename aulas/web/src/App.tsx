13b5557d1afb75420d55b4c44bcb0d0c03a841f7
Lucas  Rodrigues Estefano <42548142+lucasg13info@users.noreply.github.com>
2020-06-03 21:17:41 -0300

    Inserindo google fonts

--- aulas/web/src/App.tsx
+++ aulas/web/src/App.tsx
@@ -1,23 +1,11 @@
-import React, {useState} from 'react';
+import React from 'react';
 import './App.css';
 
-import Header from './Header';
+
 
 function App() {
-    const [counter, setCounter] = useState(0);
-
-    function handleButtonClick(){
-        setCounter(counter + 1);
-    }
-
-  return (
-        <div>
-          <Header title={`Contador : ${counter}`} />
- 
-          <h1>{counter}</h1>
-
-          <button type="button" onClick={handleButtonClick}>Aumentar</button>
-        </div>
+    return (
+        <h1>Hello World</h1>
   );
 }
 export default App;
