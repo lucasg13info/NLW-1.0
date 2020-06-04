1b9607e15015acc8105ab11d21bdbdc62eb69048
Lucas  Rodrigues Estefano <42548142+lucasg13info@users.noreply.github.com>
2020-06-03 22:21:11 -0300

    Criando segunda pag

--- aulas/web/src/App.tsx
+++ aulas/web/src/App.tsx
@@ -2,12 +2,13 @@ import React from 'react';
 import './App.css';
 
 import Home from './pages/Home';
+import CreatePoint from './pages/CreatePoint'
 
 
 
 function App() {
     return (
-        <Home />
+        <CreatePoint />
   );
 }
 export default App;
