5aaf2b0e433d5b98610d5199e9f2eaa6fbfd25c3
Lucas  Rodrigues Estefano <42548142+lucasg13info@users.noreply.github.com>
2020-06-03 22:13:18 -0300

    Voltando onde estavamos antes do bug

--- aulas/web/src/pages/Home/index.tsx
+++ aulas/web/src/pages/Home/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import './styles.css';
 
 import logo from '../../assets/logo.svg';
 
@@ -6,7 +7,20 @@ const Home = () => {
     return (
        <div id="page-home">
            <div className="content">
-                <img src={logo} alt='Ecoleta' />
+                <header>
+                 <img src={logo} alt='Ecoleta' />
+                </header>
+
+                <main>
+                    <h1>Seu marketplace de coleta de resíduos.</h1>
+                    <p>Ajudamos pessoas a encontrarem pontos de coleta de forma eficiente.</p>
+                    <a href="/cadastro">
+                        <span>
+                            >
+                        </span>
+                        <strong>Cadastre um ponto de coleta</strong>
+                    </a>
+                </main>
            </div>
        </div>
     );
