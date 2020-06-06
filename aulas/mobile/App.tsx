fa6574eb0bd017b7f8a384f6a087d25e57d143c9
Lucas  Rodrigues Estefano <42548142+lucasg13info@users.noreply.github.com>
2020-06-05 23:26:38 -0300

    Corrigindo bugs

--- aulas/mobile/App.tsx
+++ aulas/mobile/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { AppLoading} from 'expo';
+import { AppLoading } from 'expo';
 import { StatusBar} from 'react-native';
 import { Roboto_400Regular, Roboto_500Medium} from '@expo-google-fonts/roboto'
 import {Ubuntu_700Bold, useFonts} from '@expo-google-fonts/ubuntu'
